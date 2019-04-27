14ae84d938b4aaa3ebf777419f6290a74475cc37
Flykun <1689890728@qq.com>
2019-04-27 13:19:30 +0800

    增加了距离计算

--- myLocation/myLoc.js
+++ myLocation/myLoc.js
@@ -1,5 +1,12 @@
 window.onload = getMyLocation;
 
+// 总部坐标
+var ourCoords = {
+    latitude: 47.624851,
+    longitude: -122.52099,
+};
+
+// 获取位置信息
 function getMyLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(displayLocation, displayError);
@@ -8,13 +15,19 @@ function getMyLocation() {
     }
 }
 
+// 显示坐标
 function displayLocation(position) {
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
     var div = document.getElementById("location");
-    div.innerHTML = "You are at Latitude: " + latitude + ", longitude: " + longitude
+    div.innerHTML = "You are at Latitude: " + latitude + ", longitude: " + longitude;
+    // 传递坐标
+    var km = computeDistance(position.coords, ourCoords);
+    var distance = document.getElementById("distance");
+    distance.innerHTML = "You are " + km + " km from the WickedlySmart HQ";
 }
 
+//显示错误信息
 function displayError(error) {
     var errorTypes = {
         0: "Unknow error",
@@ -29,5 +42,27 @@ function displayError(error) {
     }
     var div = document.getElementById("location");
     div.innerHTML = errorMessage;
+
 }
 
+// 计算距离
+function computeDistance(startCoords, destCoords) {
+    var startLatRads = degreesToRadians(startCoods.latitude);
+    var startLongRads = degreesToRadians(startCoords.longitude);
+    var destLatRads = degreesToRadians(startCoords.latitude);
+    var destLongRads = degreesToRadians(startCoords.longitude);
+
+    var Radius = 6371; // 地球半径(km)
+    var distance = Math.acos(
+        Math.sin(startLatRads) * Math.sin(destLatRads)
+        + Math.cos(startLatRads) * Math.cos(destLatRads) 
+        * Math.cos(startLongRads - destLongRads)
+    ) * Radius;
+    return distance;
+}
+
+// 角度转弧度
+function degreesToRadians(degrees) {
+    var radians = (degrees * Math.PI) / 180;
+    return radians;
+}
